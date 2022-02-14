1d78cef95d0d43201565db7e9624dffafd740fc4
DarkAnder123 <kingandriy@mail.ru>
2022-02-14 16:31:40 +0200

    changed name

--- src/books/books.controller.interface.ts
+++ src/books/books.controller.interface.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 
-export interface BooksControllerInterface {
+export interface IBooksControllerInterface {
 	getAllBooks: (req: Request, res: Response, next: NextFunction) => void;
 	getBook: (req: Request, res: Response, next: NextFunction) => void;
 	deleteBook: (req: Request, res: Response, next: NextFunction) => void;
